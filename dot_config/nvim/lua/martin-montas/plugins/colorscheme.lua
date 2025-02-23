15663cf0b42871ed8342f49e620e3d11f5288e93
Martin Montas <martinmontas1@gmail.com>
2025-02-22 20:45:32 -0500

    Adding extra things for now

--- dot_config/nvim/lua/martin-montas/plugins/colorscheme.lua
+++ dot_config/nvim/lua/martin-montas/plugins/colorscheme.lua
@@ -1,7 +1,9 @@
 return {
-    	'folke/tokyonight.nvim',
+    'rockerBOO/boo-colorscheme-nvim',
 	config = function() 
-        vim.api.nvim_command('colorscheme tokyonight-night')
-        --vim.api.nvim_command('highlight  Normal guibg=None')
-	end
+        vim.api.nvim_command('colorscheme forest_stream')
+        vim.api.nvim_command('highlight  Normal guibg=#0d1914')
+        vim.api.nvim_command('highlight CursorLine guibg=#990000')
+        vim.api.nvim_command('highlight  StatusLine guibg=#838996 guifg=#101010')
+    end
 }
